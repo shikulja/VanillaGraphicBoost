3933b84a08c43cc2ce39f172cfea3de690bb44d0
shikulja <gamemazahist@gmail.com>
2024-05-29 21:40:12 +0300

    Parameter switching function
    Parameter switching function.
    Backported from ShaguTweaks-mods

--- VanillaGraphicBoost.lua
+++ VanillaGraphicBoost.lua
@@ -2,9 +2,9 @@
 SlashCmdList["CONSOLE"]("anisotropic 16");
 SlashCmdList["CONSOLE"]("baseMip 0");
 SlashCmdList["CONSOLE"]("detailDoodadAlpha 100");
-SlashCmdList["CONSOLE"]("DistCull 888");
+--SlashCmdList["CONSOLE"]("DistCull 888");
 SlashCmdList["CONSOLE"]("doodadAnim 1");
-SlashCmdList["CONSOLE"]("farclip 777");
+--SlashCmdList["CONSOLE"]("farclip 777"); -- terrain render distance. Default game value is 777
 SlashCmdList["CONSOLE"]("ffx 1");
 SlashCmdList["CONSOLE"]("ffxDeath 1");
 SlashCmdList["CONSOLE"]("ffxGlow 1");
@@ -15,7 +15,7 @@ SlashCmdList["CONSOLE"]("gxColorBits 24");
 SlashCmdList["CONSOLE"]("gxDepthBits 24");
 SlashCmdList["CONSOLE"]("horizonfarclip 2112");
 SlashCmdList["CONSOLE"]("lod 0");
-SlashCmdList["CONSOLE"]("lodDist 250");
+--SlashCmdList["CONSOLE"]("lodDist 250");
 SlashCmdList["CONSOLE"]("mapObjLightLOD 2");
 SlashCmdList["CONSOLE"]("mapObjOverbright 1");
 SlashCmdList["CONSOLE"]("mapShadows 1");
@@ -65,3 +65,39 @@ if IsSuperWoWLoaded() then
 	SlashCmdList["CONSOLE"]("SoundSoftwareChannels 64"); -- If you want true uncapped sound experience you still have accompany this CVAR
 	SlashCmdList["CONSOLE"]("SoundMaxHardwareChannels 64"); -- If you want true uncapped sound experience you still have accompany this CVAR
 end
+
+	-- https://github.com/GryllsAddons/ShaguTweaks-mods/blob/main/mods/max-graphics.lua
+	-- https://docs.google.com/spreadsheets/d/17bXs9WhOkP8Zknl1GYXCFVdHYOdgxoRFrIe7Os3BtRo/edit#gid=0
+	local graphics = CreateFrame("Frame", nil, UIParent)
+
+	function graphics:performance()
+		SlashCmdList["CONSOLE"]("DistCull 500") -- 500 / 888
+		SlashCmdList["CONSOLE"]("farclip 500") -- 500 / 777
+		SlashCmdList["CONSOLE"]("lodDist 100") -- 100 / 250
+	end
+
+	function graphics:quality()
+		SlashCmdList["CONSOLE"]("DistCull 888") -- 500 / 888
+		SlashCmdList["CONSOLE"]("farclip 777") -- 500 / 777
+		SlashCmdList["CONSOLE"]("lodDist 250") -- 100 / 250
+	end
+
+	function graphics:mapUpdate()
+		local zone = GetRealZoneText()
+		if graphics.zone ~= zone then
+			graphics.zone = zone
+			if IsInInstance() then
+				graphics:performance()
+			else
+				graphics:quality()
+			end
+		end
+	end
+
+	graphics:RegisterEvent("PLAYER_ENTERING_WORLD")
+	graphics:RegisterEvent("MINIMAP_ZONE_CHANGED")
+	graphics:SetScript("OnEvent", function()
+		if event == "MINIMAP_ZONE_CHANGED" then
+			graphics:mapUpdate()
+		end
+	end)
